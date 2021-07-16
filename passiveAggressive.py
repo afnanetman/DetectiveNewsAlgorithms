3a3a7babb6f26e9764324d638e14e8ddbcb2a5db
esraa2367 <36345039+esraa2367@users.noreply.github.com>
2021-07-16 21:10:43 +0200

    add detect article page

--- passiveAggressive.py
+++ passiveAggressive.py
@@ -5,8 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
-from newsapi import NewsApiClient
-from newspaper import Article
+#from newsapi import NewsApiClient
+#from newspaper import Article
 import re
 from detectiveNewsSystem import detectiveNewsSystem
 
