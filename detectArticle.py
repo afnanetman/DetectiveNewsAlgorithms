085e67cc0d72f2676c49f3100dbeb8247467d347
afnanetman <36138072+afnanetman@users.noreply.github.com>
2021-07-25 16:27:31 +0200

    last

--- detectArticle.py
+++ detectArticle.py
@@ -2,30 +2,22 @@ from flask import Flask, jsonify, request
 import json
 from passiveAggressive import passiveAgressiveClassifier 
 
-#declared an empty variable for reassignment
-response = ''
-
 #creating the instance of our flask application
 app = Flask(__name__)
 
-#route to entertain our post and get request from flutter app
+#route to entertain our post request from flutter app
 @app.route('/article', methods =['POST'])
 def nameRoute():
 
-    #fetching the global response variable to manipulate inside the function
-    global response
-
-    #checking the request type we get from the app
-    request_data = request.data #getting the response data
-    request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair
-    article = request_data['article'] #assigning it to name
+    request_data = request.data #getting the article
+    request_data = json.loads(request_data.decode('utf-8')) #converting it from json to key value pair {'article': 'input article from flutter'}
+    article = request_data['article'] #assign Key of article to article variable
     print(article)
     news=[['null','null','null',article]]
     Pac = passiveAgressiveClassifier
     result = Pac.newsDetection(Pac,news)
     print(result[0])
-    #response = print(result[0]) #re-assigning response with the name we got from the user
-    return jsonify({'article' : result[0]}) #sending data back to your frontend app
+    return jsonify({'article' : result[0]}) #sending data back to your frontend app {'article' : fake / real} in json type
 
-if __name__ == "__main__":
+if __name__ == "__main__": #is used to execute some code only if the file was run directly, and not imported.
     app.run(debug=True)
